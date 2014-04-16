d907e88e3f0b956638c77234860853db0f7c6fde
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-04-16 15:24:50 +0900

    Add action check

--- Fav_And_ReTweet.rb
+++ Fav_And_ReTweet.rb
@@ -6,9 +6,7 @@ Plugin.create(:Fav_And_ReTweet) do
     condition: Plugin::Command[:HasOneMessage],
     visible: true,
     role: :timeline) do |opt|
-      message = opt.messages.first
-      screen_name = message.user[:idname]
-      Gtk::Clipboard.copy("#{screen_name}: #{message.to_s} [https://twitter.com/#{screen_name}/status/#{message.id}]")
+    Gtk::Clipboard.copy("Fav And ReTweet")
   end
   
 end
